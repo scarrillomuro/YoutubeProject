2137247a3eebc44bdfceea8056832010ede340e2
Samuel Carrillo <Mr.Carrillo@Sandys-MacBook-Air.local>
2023-04-26 21:46:34 -0500

    Initial commit

---
+++ src/EnemyShip.java
@@ -0,0 +1,35 @@
+public class EnemyShip {
+
+    private String name;
+    private double amtDamage;
+
+    public String  getName() {
+        return name;
+    }
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    public Double getDamage()  {
+        return amtDamage;
+    }
+
+    public void setDamage(double newDamage) {
+        amtDamage = newDamage;
+    }
+
+    public void followHeroShip() {
+        System.out.println(getName() + " is following the hero");
+    }
+
+    public void displayEnemyShip() {
+
+        System.out.println(getName() + "  is on the screen");
+    }
+
+    public void enemyShitShoots() {
+
+        System.out.println(getName() + " attacks and does " + getDamage());
+    }
+
+}
